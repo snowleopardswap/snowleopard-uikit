56a23c55c62eed17676f43de58d089d4cba1195e
snowleopardswap <84729476+snowleopardswap@users.noreply.github.com>
2021-05-24 18:09:33 +0200

    Add files via upload

---
+++ src/components/Svg/Icons/TicketRound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 48 48" {...props}>
+        <image width={48} height={48} href="/images/egg/ticket.png"/>
+    </Svg>
+  );
+};
+
+export default Icon;
