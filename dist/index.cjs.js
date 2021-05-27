e2fae86b546d096558f0fe3015349d084341ec57
snowleopardswap <84729476+snowleopardswap@users.noreply.github.com>
2021-05-27 17:14:42 +0200

    Update index.cjs.js

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2150,7 +2150,7 @@ var socials = [
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/FinanceGoose",
+        href: "https://twitter.com/snowleopardswap",
     },
 ];
 var MENU_HEIGHT = 64;
